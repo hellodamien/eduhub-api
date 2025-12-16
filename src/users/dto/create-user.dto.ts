683807901a0faf0d582723bdb7e41289241a1a68
Damien Pereira <damien.pereira@sysentive.com>
2025-12-16 08:09:25 +0100

    fix prisma imports

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsEnum, IsString } from 'class-validator';
-import { Role } from '@prisma/client';
+import { Role } from 'generated/prisma';
 
 export class CreateUserDto {
   @IsString()
